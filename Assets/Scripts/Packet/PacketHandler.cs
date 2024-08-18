18a006c3a68912eca9fb0ff4ee854c0fbce4e420
dokilove2@gmail.com <tulmooner2@>
2024-08-18 17:45:42 +0900

    섹션 2 : 데미지 판정

--- Assets/Scripts/Packet/PacketHandler.cs
+++ Assets/Scripts/Packet/PacketHandler.cs
@@ -70,4 +70,23 @@ class PacketHandler
             pc.UseSkill(skillPacket.Info.SkillId);
         }
     }
+
+    public static void S_ChangeHpHandler(PacketSession session, IMessage packet)
+    {
+        S_ChangeHp changePacket = packet as S_ChangeHp;
+        ServerSession serverSession = session as ServerSession;
+
+        GameObject go = Managers.Object.FindById(changePacket.ObjectId);
+
+        if (go == null)
+            return;
+
+        CreatureController cc = go.GetComponent<CreatureController>();
+        if (cc != null)
+        {
+            cc.Stat.Hp = changePacket.Hp;
+            // TODO : UI
+            Debug.Log($"ChangeHP : {changePacket.Hp}");
+        }
+    }
 }
