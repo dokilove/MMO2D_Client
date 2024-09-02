0a0c34719c81be39d0d1a85a7287f90f13a8a728
dokilove2@gmail.com <tulmooner2@>
2024-09-02 21:25:34 +0900

    섹션 2 : Die Effect

--- Assets/Scripts/Packet/PacketHandler.cs
+++ Assets/Scripts/Packet/PacketHandler.cs
@@ -17,7 +17,7 @@ class PacketHandler
     public static void S_LeaveGameHandler(PacketSession session, IMessage packet)
     {
         S_LeaveGame leaveGamePacket = packet as S_LeaveGame;
-        Managers.Object.RemoveMyPlayer();
+        Managers.Object.Clear();
     }
     public static void S_SpawnHandler(PacketSession session, IMessage packet)
     {
@@ -47,11 +47,11 @@ class PacketHandler
         if (go == null)
             return;
 
-        CreatureController cc = go.GetComponent<CreatureController>();
-        if (cc == null)
+        BaseController bc = go.GetComponent<BaseController>();
+        if (bc == null)
             return;
 
-        cc.PosInfo = movePacket.PosInfo;
+        bc.PosInfo = movePacket.PosInfo;
     }
 
     public static void S_SkillHandler(PacketSession session, IMessage packet)
@@ -85,6 +85,23 @@ class PacketHandler
         if (cc != null)
         {
             cc.Hp = changePacket.Hp;
+        }
+    }
+    public static void S_DieHandler(PacketSession session, IMessage packet)
+    {
+        S_Die diePacket = packet as S_Die;
+        ServerSession serverSession = session as ServerSession;
+
+        GameObject go = Managers.Object.FindById(diePacket.ObjectId);
+
+        if (go == null)
+            return;
+
+        CreatureController cc = go.GetComponent<CreatureController>();
+        if (cc != null)
+        {
+            cc.Hp = 0;
+            cc.OnDead();
         }
     }
 }
